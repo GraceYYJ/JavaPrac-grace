8b42b3265910af49edf72b3b3d545a6016dfbcb0
GraceYYJ <649673739@qq.com>
2017-12-11 10:05:32 +0800

    38

--- src/jianzhiOffer/Permutation38.java
+++ src/jianzhiOffer/Permutation38.java
@@ -6,22 +6,41 @@ import java.util.ArrayList;
  * Created by Administrator on 2017/12/9.
  */
 public class Permutation38 {
-  /*  ArrayList<String> arrayList=new ArrayList<String>();
+    ArrayList<String> arrayList=new ArrayList<String>();
+    StringBuffer buffer;
     public ArrayList<String> Permutation(String str) {
+        if(str==null||str.length()<=0)
+            return arrayList;
         int index=0;
-        if(str.length()<=0)
-            return null;
+        buffer=new StringBuffer(str);
+        PermutationCore(0);
+        return arrayList;
     }
-    public void Permutation(String str,int index){
-        if(index==str.length()-1){
-            arrayList.add(str);
+    public void PermutationCore(int index){
+        if(index==buffer.length()-1){
+            arrayList.add(buffer.toString());
         }
         else {
-            for(int i=index;i<str.length();i++){
-                char temp=str.charAt(i);
-                str.
+            for(int i=index;i<buffer.length();i++){
+                swap(i,index);
+                PermutationCore(index+1);
+                swap(i,index);
             }
         }
-    }*/
+    }
+    public void swap(int i,int j){
+        char a=buffer.charAt(i);
+        char b=buffer.charAt(j);
+        buffer.setCharAt(i,b);
+        buffer.setCharAt(j,a);
+    }
 
+    public static void main(String[] args) {
+        String str1=null;
+        Permutation38 test=new Permutation38();
+        ArrayList<String> lists=test.Permutation(str1);
+        for(int i=0;i<lists.size();i++){
+            System.out.println(lists.get(i));
+        }
+    }
 }
